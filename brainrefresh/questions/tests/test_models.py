a73a050c6cf961536c8580c9bf98c124345f1ff8
Dmitry G <sdprecovery@gmail.com>
2023-02-08 17:05:00 +0300

    Tests for models - QuestionTests, ChoiceTests

--- brainrefresh/questions/tests/test_models.py
+++ brainrefresh/questions/tests/test_models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 
 from brainrefresh.users.tests.factories import UserFactory
 
-from .factories import Question, QuestionFactory, Tag, TagFactory
+from .factories import Choice, ChoiceFactory, Question, QuestionFactory, Tag, TagFactory
 
 
 class TagTests(TestCase):
@@ -65,51 +65,100 @@ class QuestionTests(TestCase):
     def setUp(self):
         self.user = UserFactory(username="testuser", password="12345")
         self.tag = TagFactory(label="test_tag")
-        self.question = QuestionFactory(
-            user=self.user,
-            title="Test Question Title",
-            text="Test Question Text",
-            explanation="Test explanation",
-            language=Question.Lang.EN,
-            published=True,
-        )
+        self.question_data = {
+            "user": self.user,
+            "title": "Test Question Title",
+            "text": "Test Question Text",
+            "explanation": "Test explanation",
+            "language": Question.Lang.EN,
+            "published": True,
+        }
+        self.question = QuestionFactory(**self.question_data)
 
     def test_create(self):
         self.assertEqual(Question.objects.count(), 1)
-        self.assertEqual(self.question.title, "Test Question Title")
-        self.assertEqual(self.question.text, "Test Question Text")
-        self.assertEqual(self.question.explanation, "Test explanation")
-        self.assertEqual(self.question.language, Question.Lang.EN)
-        self.assertTrue(self.question.published)
 
     def test_update(self):
-        self.question.title = "Updated test question"
+        new_title = "Updated test question"
+        self.question.title = new_title
         self.question.save()
         self.question.refresh_from_db()
         self.assertEqual(Question.objects.count(), 1)
-        self.assertEqual(self.question.title, "Updated test question")
-        self.assertEqual(self.question.text, "Test Question Text")
-        self.assertEqual(self.question.explanation, "Test explanation")
-        self.assertEqual(self.question.language, Question.Lang.EN)
-        self.assertTrue(self.question.published)
+        self.assertEqual(self.question.title, new_title)
 
     def test_delete(self):
         self.question.delete()
         self.assertEqual(Question.objects.count(), 0)
 
     def test_str(self):
-        self.assertEqual(str(self.question), "Test Question Title")
+        self.assertEqual(str(self.question), self.question_data["title"])
 
     def test_fields(self):
-        self.question.tags.add(self.tag)
-        self.question.refresh_from_db()
-        # assert fields are correctly saved and retrieved
-        self.assertEqual(self.question.user, self.user)
-        self.assertEqual(self.question.title, "Test Question Title")
-        self.assertEqual(self.question.text, "Test Question Text")
+        self.assertEqual(self.question.title, self.question_data["title"])
+        self.assertEqual(self.question.text, self.question_data["text"])
         self.assertEqual(self.question.language, Question.Lang.EN)
         self.assertEqual(self.question.published, True)
         self.assertIsInstance(self.question.uuid, UUID)
         self.assertIsInstance(self.question.created_at, timezone.datetime)
         self.assertIsInstance(self.question.updated_at, timezone.datetime)
+
+    def test_related_fields(self):
+        self.question.tags.add(self.tag)
+        self.question.refresh_from_db()
+        # tests
+        self.assertEqual(self.question.user, self.user)
         self.assertIn(self.tag, self.question.tags.all())
+
+
+class ChoiceTests(TestCase):
+    def setUp(self):
+        self.question_data = {
+            "title": "Test Question Title",
+            "text": "Test Question Text",
+            "explanation": "Test explanation",
+            "language": Question.Lang.EN,
+            "published": True,
+        }
+        self.question = QuestionFactory(**self.question_data)
+        self.choice_data = {
+            "question": self.question,
+            "text": "Test Choice",
+            "is_correct": False,
+        }
+        self.choice = ChoiceFactory(**self.choice_data)
+        ChoiceFactory.create_batch(5, question=self.question)
+
+    def test_create(self):
+        self.assertEqual(Choice.objects.count(), 6)
+
+    def test_update(self):
+        new_text = "Updated test question"
+        self.choice.text = new_text
+        self.choice.save()
+        self.choice.refresh_from_db()
+        self.assertEqual(self.choice.text, new_text)
+
+    def test_delete(self):
+        self.choice.delete()
+        self.assertEqual(Choice.objects.count(), 5)
+
+    def test_fields(self):
+        self.assertIsInstance(self.choice, Choice)
+        self.assertIsInstance(self.choice.uuid, UUID)
+        self.assertEqual(self.choice.text, self.choice_data["text"])
+        self.assertFalse(self.choice.is_correct)
+
+    def test_str(self):
+        self.assertEqual(
+            str(self.choice), f"{self.question.title} : {str(self.choice.uuid)}"
+        )
+
+    def test_related_fields(self):
+        self.assertIsInstance(self.choice.question, Question)
+        self.assertEqual(self.choice.question, self.question)
+
+    def test_delete_cascade(self):
+        choice_id = self.choice.id
+        self.question.delete()
+        with self.assertRaises(Choice.DoesNotExist):
+            Choice.objects.get(id=choice_id)
